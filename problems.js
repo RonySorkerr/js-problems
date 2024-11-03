9c97504885c3ff9ec84f8f2e1430bc367e9b67b2
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-03 22:36:20 +0600

    Second Problem Done

--- problems.js
+++ problems.js
@@ -1,7 +1,15 @@
-let a = -1;
-if(a<0){
-    console.log("The number is Negative");
+// let a = -1;
+// if(a<0){
+//     console.log("The number is Negative");
+// }
+// else{
+//     console.log("the Number Is Positive");
+// }
+
+let q = 3;
+if (q % 2== 0) {
+    console.log("the number is even");
 }
 else{
-    console.log("the Number Is Positive");
+    console.log("The Number Is odd");
 }
