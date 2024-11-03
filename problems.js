e3610756e89f341d2b7c270b2e1f9fa1c7eae16e
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-03 23:00:33 +0600

    fourth problem is done

--- problems.js
+++ problems.js
@@ -17,22 +17,31 @@
 // }
 
 // Problem * 03
-let age = 15;
-if (age <= 12) {
-    console.log("Your Ticket Price is : 5 %");
+// let age = 15;
+// if (age <= 12) {
+//     console.log("Your Ticket Price is : 5 %");
+// }
+// else {
+//     if (age <= 18) {
+//         console.log("Your Ticket Price is : 10 $");
+//     }
+//     else {
+//         if (age <= 60) {
+//             console.log("Your Ticket Price is : 20 $");
+//         }
+//         else{
+//             if(age >= 60){
+//                 console.log("Your Ticket Price is : 15 $");
+//             }
+//         }
+//     }
+// }
+
+// Problem - *04
+let year = 2006;
+if (year % 4 == 0){
+    console.log("The Year Is A leap year");
 }
-else {
-    if (age <= 18) {
-        console.log("Your Ticket Price is : 10 $");
-    }
-    else {
-        if (age <= 60) {
-            console.log("Your Ticket Price is : 20 $");
-        }
-        else{
-            if(age >= 60){
-                console.log("Your Ticket Price is : 15 $");
-            }
-        }
-    }
+else{
+    console.log("The year isn't a leap year");
 }
