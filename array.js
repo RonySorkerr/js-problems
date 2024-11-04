65b4945fa63d1844083621597a5c9f717345a061
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-04 23:16:06 +0600

    array method

---
+++ array.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5,6];
+console.log(numbers.length);
+if(numbers.includes(6)){
+    console.log("You are in the right room");
+}
+else{
+    console.log("you are in the wrong room");
+}
