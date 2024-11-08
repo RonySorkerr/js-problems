40d0f20d273af09afc90e55d56dfeedc1348a3ed
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-08 18:49:12 +0600

    fine

---
+++ loop.js
@@ -0,0 +1,13 @@
+// const names = ['rhim','karim','jabbar','ratan'];
+
+// for(const name of names){
+//     console.log(name);
+// }
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum += i;
+//     console.log(i);
+// }
+// console.log(sum);
+
+ 
