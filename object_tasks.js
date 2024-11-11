ca9647cde8a167ce428f7d8b2db20121cf828fd3
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-11 22:36:47 +0600

    Task - 2 Done

--- object_tasks.js
+++ object_tasks.js
@@ -1,8 +1,20 @@
-const colors = {
-    red: "#ff0000",
-    green: "#00ff00",
-    blue: "#0000ff",
-    "golden rod": '#daa520'
+// Task - 01
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+
+// console.log(colors["golden rod"]);
+
+// Task - 02
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
 };
 
-console.log(colors["golden rod"]);
+car.passengerCapacity = 5;
+
+console.log(car);
