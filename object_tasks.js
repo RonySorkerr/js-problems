1af5154864d0e8a30c7021b2fb7f770c5997edc7
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-11 22:39:27 +0600

    task 3 done

--- object_tasks.js
+++ object_tasks.js
@@ -9,12 +9,25 @@
 // console.log(colors["golden rod"]);
 
 // Task - 02
-const car = {
-    make: "Toyota",
-    model: "Corolla",
-    year: 2020
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+
+// car.passengerCapacity = 5;
+
+// console.log(car);
+
+// Task - 03
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
 };
-
-car.passengerCapacity = 5;
-
-console.log(car);
+console.log(student.physics.marks);
