713ff59160e813a8a202b3c56fe6b4d6e50050c5
RonySorkerr <mdronysarkar33299@gmail.com>
2024-11-11 22:42:43 +0600

    task 4 done

--- object_tasks.js
+++ object_tasks.js
@@ -21,13 +21,26 @@
 
 // Task - 03
 
-const student = {
-    name: "Hamim Sakep",
-    id: 5421,
-    physics: {
-        subject: "HSC Physics",
-        author: "Shahjahan Tapan",
-        marks: 30
-    }
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks);
+
+// Task - 4
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
 };
-console.log(student.physics.marks);
+
+const numOfStudent = Object.keys(student).length;
+
+console.log(numOfStudent);
